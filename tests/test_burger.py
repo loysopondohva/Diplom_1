2f6ff0b45c410c80a6c5446eb32e88d18728079e
loysopondohva <loysopondohva@yandex.com>
2025-08-10 11:44:16 +0300

    Дополнения с описанием тестов

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -5,28 +5,34 @@ from ..praktikum.burger import Burger
 
 class TestBurger(object):
 
+# Проверка инициализации класса
     def test_init_burger(self, burger):
         assert burger.bun == None
         assert burger.ingredients == []
 
+# Проверка установки булочки
     def test_set_buns(self, burger, bun):
         burger.set_buns(bun)
         assert burger.bun == bun
 
+# Проверка добавления ингридиентов в бургер
     def test_add_ingredient(self, burger, ingredient):
         burger.add_ingredient(ingredient)
         assert burger.ingredients == [ingredient]
 
+# Проверка удаления ингридиента из бургера
     def test_remove_ingredient(self, burger, ingredient):
         burger.add_ingredient(ingredient)
         burger.remove_ingredient(0)
         assert burger.ingredients == []
 
+# Проверка удаления ингридиента с некорректным индексом
     def test_remove_invalid_ingredient(self, burger, ingredient):
         burger.add_ingredient(ingredient)
         with pytest.raises(IndexError):
             burger.remove_ingredient(3)
 
+# Проверка перемещения ингридиентов
     def test_move_ingredient(self, burger):
         ingredient1_mock = Mock()
         ingredient1_mock.get_name.return_value = "Cheese"
@@ -41,6 +47,7 @@ class TestBurger(object):
         burger.move_ingredient(1,0)
         assert burger.ingredients == [ingredient2_mock, ingredient1_mock]
 
+# Проверка расчёта стоимости бургера
     @pytest.mark.parametrize("ingredients, expected_price", [
         ([("FILLING", "Cheese", 70.0), ("SAUCE", "Ketchup", 25.0)], 195.0),
         ([("FILLING", "Red Hot Chili Peppers", 20.0), ("FILLING", "Tomato", 30.0), ("SAUCE", "BBQ", 20.0)], 170.0)
@@ -55,6 +62,7 @@ class TestBurger(object):
             burger.add_ingredient(ingredient_mock)
         assert burger.get_price() == expected_price
 
+# проверка генерации рецепта
     def test_get_receipt(self, burger, bun, ingredient):
         burger.set_buns(bun)
         burger.add_ingredient(ingredient)
